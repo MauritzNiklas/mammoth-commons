92e6a772471ad7562b6aabbb47d6c31a4f35df97
MauritzNiklas <61232611+MauritzNiklas@users.noreply.github.com>
2024-09-17 18:56:24 +0200

    Upload of ma_graph_connection metric, and update of old ones

--- mammoth/datasets/graph_csh.py
+++ mammoth/datasets/graph_csh.py
@@ -1,23 +1,24 @@
 from mammoth.datasets.dataset import Dataset
-import networkx as nx
 
 
 class Graph_CSH(Dataset):
-    def __init__(self, nodes_list, edges_list, attributes_list):
-        self.nodes_list = nodes_list
-        self.edges_list = edges_list
+    def __init__(self, nodes_df, edges_df, attributes_list):
+        import networkx as nx
+        self.nodes_df = nodes_df
+        self.edges_df = edges_df
         self.attributes_list = attributes_list
         self.G = None
 
     def create_graph(self):
+        import networkx as nx
         self.G = nx.DiGraph()
-        nodes_for_graph = [(int(i),{attribute:self.nodes_list.loc[i,attribute] for attribute in self.attributes_list}) for i in self.nodes_list.index]
-        edges_for_graph = [(int(self.edges_list.iloc[i,0]),int(self.edges_list.iloc[i,1])) for i in range(self.edges_list.shape[0])]
+        nodes_for_graph = [(int(i),{attribute:self.nodes_df.loc[i,attribute] for attribute in self.attributes_list}) for i in self.nodes_df.index]
+        edges_for_graph = [(int(self.edges_df.iloc[i,0]),int(self.edges_df.iloc[i,1])) for i in range(self.edges_df.shape[0])]
         self.G.add_nodes_from(nodes_for_graph)
         self.G.add_edges_from(edges_for_graph)
 
     def return_num_nodes(self):
-        return self.nodes_list.shape[0]
+        return self.nodes_df.shape[0]
     
     def return_num_edges(self):
-        return self.edges_list.shape[0]
+        return self.edges_df.shape[0]
